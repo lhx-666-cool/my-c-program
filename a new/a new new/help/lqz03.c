359601cd0f07753f4523e0c1161a13800af20cef
lhx-666-cool <d3032471065@yeah.net>
2022-12-14 20:19:16 +0800

    12-14

--- a new/a new new/help/lqz03.c
+++ a new/a new new/help/lqz03.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 void sortScore(int* score, int n) {
     int temp;
-    for (int i = 0;i < n - 1;i++) {
-        for (int j = i + 1;j < n;j++) {
+    int i,j; 
+    for ( i = 0;i < n - 1;i++) {
+        for ( j = i + 1;j < n;j++) {
             if (score[i] > score[j]) {
                 temp = score[i];
                 score[i] = score[j];
@@ -10,13 +11,14 @@ void sortScore(int* score, int n) {
             }
         }
     }
-    for (int i = 0;i < n;i++) {
+    for ( i = 0;i < n;i++) {
         printf("%d ", score[i]);
     }
 }
 void counter(int* score, int n) {
     int count = 0;
-    for (int i = 0;i < n;i++) {
+    int i;
+    for (i = 0;i < n;i++) {
         if (score[i] < 60) {
             count++;
         }
@@ -27,7 +29,8 @@ int main() {
     int n;
     int score[100] = { 0 };
     scanf("%d", &n);
-    for (int i = 0;i < n;i++) {
+    int i;
+    for (i = 0;i < n;i++) {
         scanf("%d", &score[i]);
     }
     sortScore(score, n);
