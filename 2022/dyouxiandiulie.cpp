d6f16853da8585d854dccebd19fc8851e104f691
lhx-666-cool <d3032471065@yeah.net>
2023-01-01 18:31:11 +0800

    23-1-1

--- 2022/dyouxiandiulie.cpp
+++ 2022/dyouxiandiulie.cpp
@@ -1,15 +1,48 @@
 #include<cstdio>
-#include<queue>
 using namespace std;
-priority_queue<int>q;
-int gezi[1000000] = { 0 };
+const int N = 2e6 + 10;
+int gezi[N];
+int q[N];
+int step[N];
 int main() {
-    int n;
+    int n, m;
+    int hh = 0, tt = -1;
+    scanf("%d %d", &n, &m);
     for (int i = 1;i <= n;i++) {
         scanf("%d", &gezi[i]);
     }
-    q.push(0);
+    q[++tt] = n;
     for (int i = n - 1;i >= 0;i--) {
 
+        while (hh <= tt && i + m < q[hh]) {
+            hh++;
+        }
+        while (hh <= tt && step[q[tt]] < step[i + 1]) {
+            tt--;
+        }
+        q[++tt] = i + 1;
+        if (gezi[i] == 0) {
+            step[i] = step[q[hh]] + 1;
+        } else {
+            step[i] = step[i + gezi[i]];
+        }
     }
+    printf("%d ", step[0]);
+    hh = 0, tt = -1;
+    q[++tt] = n;
+    for (int i = n - 1;i >= 0;i--) {
+        while (hh <= tt && i + m < q[hh]) {
+            hh++;
+        }
+        while (hh <= tt && step[q[tt]] > step[i + 1]) {
+            tt--;
+        }
+        q[++tt] = i + 1;
+        if (gezi[i] == 0) {
+            step[i] = step[q[hh]] + 1;
+        } else {
+            step[i] = step[i + gezi[i]];
+        }
+    }
+    printf("%d ", step[0]);
 }
